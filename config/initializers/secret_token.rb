b25d8a80c42ef4dea1a5fd3fdece58900b2c6290
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-09-28 14:59:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlexaWebsite::Application.config.secret_token = 'b7e2abd5899f83d402561f091e65bc44f7740d98a9ba6172582a47bd4dc05d36b180e74838a080e5332b8f039a85afebd1cce5f86f6ec1bf15c04bb4bc5ee46e'
